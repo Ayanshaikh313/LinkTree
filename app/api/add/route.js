3bdb97d0cd2ba387a61716c897512e5281c6d024
Ayan Shaikh <ayanms7861@gmail.com>
2025-02-08 16:34:03 +0530

    all done

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,21 +1,21 @@
-import clientPromise from "@/lib/mongodb"
+// import clientPromise from "@/lib/mongodb"
 
-export async function POST(request) {
-    const body= await request.json()
+// export async function POST(request) {
+//     const body= await request.json()
 
-    const client = await clientPromise;
-    const db = client.db("linkTree")
-    const collection = db.collection("links")
+//     const client = await clientPromise;
+//     const db = client.db("linkTree")
+//     const collection = db.collection("links")
 
-    //if the user is already exist then it will return an error popup
+//     //if the user is already exist then it will return an error popup
     
-    const eco = await collection.findOne({ handle: body.handle })
-    if (eco){
-        return Response.json({success: false, Error: true, message: "User already exist!" })
-    }
+//     const eco = await collection.findOne({ handle: body.handle })
+//     if (eco){
+//         return Response.json({success: false, Error: true, message: "User already exist!" })
+//     }
 
 
-    const result= await collection.insertOne(body)
+//     const result= await collection.insertOne(body)
     
-    return Response.json({success: true, Error: false, message: "Your LinkTree Has Been Generated",result:result })
-}
+//     return Response.json({success: true, Error: false, message: "Your LinkTree Has Been Generated",result:result })
+// }
